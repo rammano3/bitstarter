436c418360396ced46a77bf5169e7e789d19b2c5
rammano3 <randall.tyler.jones@gmail.com>
2013-07-10 00:36:29 +0000

    fixing buffer size

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var k = fs.readFileSync('index.html','utf8');
- buf = new Buffer(256);
+ buf = new Buffer(k.length);
  len = buf.write(k,0);
  response.send(buf.toString('utf8',0,len));
 });
