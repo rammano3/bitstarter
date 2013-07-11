7321d7ce7b43b350a76c870b6f9cdfb02275e4d4
rammano3 <randall.tyler.jones@gmail.com>
2013-07-11 13:34:40 +0000

    adding url functionality to grader and updating checks with more tags

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://safe-wave-9019.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +57,51 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var downloadURL = function(url) {
+    if(url != ""){                                                                            
+      urlfile = rest.get(url).on('complete', function(result) {                               
+       if (result instanceof Error) {                                                         
+        console.log("url does not exist. Exiting.");                                          
+        process.exit(1);                                                                      
+       }else{                                                                                 
+        program.file = 'url_dl.txt';                                                          
+        fs.writeFileSync(program.file, result.toString());                                    
+         parseFile();
+       }                                                                                      
+      });                                                                                     
+    }
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var parseFile = function(){
+    var checkJson = checkHtmlFile(program.file, program.checks);      
+    var outJson = JSON.stringify(checkJson, null, 4);                                        
+    console.log(outJson);     
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to download', '', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url != "")
+    {
+            downloadURL(program.url);
+    }else{
+      parseFile();
+    }
+    //var checkJson = checkHtmlFile(program.file, program.checks, program.url);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson+' -- '+program.file);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+
+
